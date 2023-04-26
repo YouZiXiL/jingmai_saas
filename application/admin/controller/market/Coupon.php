ca14d281c87b4ee6332b6a7b9b953a53e567428b
阿沁 <1037124449@qq.com>
2023-04-26 20:39:07 +0800

    修正二期开发

--- application/admin/controller/market/Coupon.php
+++ application/admin/controller/market/Coupon.php
@@ -5,6 +5,8 @@ namespace app\admin\controller\market;
 use app\common\controller\Backend;
 use app\web\controller\Common;
 use think\Db;
+use think\db\exception\DataNotFoundException;
+use think\db\exception\ModelNotFoundException;
 use think\Exception;
 use think\exception\DbException;
 use think\exception\PDOException;
@@ -118,6 +120,114 @@ class Coupon extends Backend
             $this->error(__('No rows were inserted'));
         }
         $this->success();
+    }
+
+
+    /**
+     * 编辑
+     *
+     * @param $ids
+     * @return string
+     * @throws DbException
+     * @throws \think\Exception
+     */
+    public function edit($ids = null)
+    {
+        $row = $this->model->get($ids);
+        if (!$row) {
+            $this->error(__('No Results were found'));
+        }
+        $adminIds = $this->getDataLimitAdminIds();
+        if (is_array($adminIds) && !in_array($row[$this->dataLimitField], $adminIds)) {
+            $this->error(__('You have no permission'));
+        }
+        if (false === $this->request->isPost()) {
+            $this->view->assign('row', $row);
+            return $this->view->fetch();
+        }
+        $params = $this->request->post('row/a');
+        if (empty($params)) {
+            $this->error(__('Parameter %s can not be empty', ''));
+        }
+        $params = $this->preExcludeFields($params);
+        $result = false;
+        Db::startTrans();
+        try {
+            //是否采用模型验证
+            if ($this->modelValidate) {
+                $name = str_replace("\\model\\", "\\validate\\", get_class($this->model));
+                $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.edit' : $name) : $this->modelValidate;
+                $row->validateFailException()->validate($validate);
+            }
+            $result = $row->allowField(true)->save(['state'=>2]);
+            $data=[
+                'agent_id'=>$row['agent_id'],
+                'name'=>$params['name']??$row['name'],
+                'gain_way'=>$params['gain_way']??$row['gain_way'],
+                'price'=>$params['price']??$row['price'],
+                'money'=>$params['money']??$row['money'],
+                'type'=>$params['type']??$row['type'],
+                'score'=>$params['score']??$row['score'],
+                'scene'=>$params['scene']??$row['scene'],
+                'uselimits'=>$params['uselimits']??$row['uselimits'],
+                'state'=>$params['state']??$row['state'],
+                'validdate'=>$params['validdate']??$row['validdate'],
+                'validdateend'=>$params['validdateend']??$row['validdateend'],
+                'limitsday'=>$params['limitsday']??$row['limitsday'],
+                'conpon_group_count'=>$params['conpon_group_count']??$row['conpon_group_count'],
+                'couponcount'=>$params['couponcount']??$row['couponcount'],
+                'create_time'=>time(),
+            ];
+            $this->model->allowField(true)->save($data);
+            Db::commit();
+        } catch (ValidateException|PDOException|Exception $e) {
+            Db::rollback();
+            $this->error($e->getMessage());
+        }
+        if (false === $result) {
+            $this->error(__('No rows were updated'));
+        }
+        $this->success();
+    }
+
+    /**
+     * 删除
+     *
+     * @param $ids
+     * @return void
+     * @throws DbException
+     * @throws DataNotFoundException
+     * @throws ModelNotFoundException
+     */
+    public function del($ids = null)
+    {
+        if (false === $this->request->isPost()) {
+            $this->error(__("Invalid parameters"));
+        }
+        $ids = $ids ?: $this->request->post("ids");
+        if (empty($ids)) {
+            $this->error(__('Parameter %s can not be empty', 'ids'));
+        }
+        $pk = $this->model->getPk();
+        $adminIds = $this->getDataLimitAdminIds();
+        if (is_array($adminIds)) {
+            $this->model->where($this->dataLimitField, 'in', $adminIds);
+        }
+
+
+
+        Db::startTrans();
+        try {
+            $list = $this->model->where($pk, 'in', $ids)->update(['state'=>2]);
+            Db::commit();
+        } catch (PDOException|Exception $e) {
+            Db::rollback();
+            $this->error($e->getMessage());
+        }
+        if ($list) {
+            $this->success();
+        }
+        $this->error(__('No rows were deleted'));
     }
 
 
