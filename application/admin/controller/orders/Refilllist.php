4d5ea8dd146878840ce5978de434537d069fab54
阿沁 <1037124449@qq.com>
2023-04-30 13:02:35 +0800

    修正风火递回调 GOBACK参数

--- application/admin/controller/orders/Refilllist.php
+++ application/admin/controller/orders/Refilllist.php
@@ -4,6 +4,8 @@ namespace app\admin\controller\orders;
 
 use app\common\controller\Backend;
 use app\web\controller\Common;
+use think\exception\DbException;
+use think\response\Json;
 
 /**
  * 
@@ -34,6 +36,34 @@ class Refilllist extends Backend
      * 因此在当前控制器中可不用编写增删改查的代码,除非需要自己控制这部分逻辑
      * 需要将application/admin/library/traits/Backend.php中对应的方法复制到当前控制器,然后进行修改
      */
+
+    /**
+     * 查看
+     *
+     * @return string|Json
+     * @throws \think\Exception
+     * @throws DbException
+     */
+    public function index()
+    {
+        //设置过滤方法
+        $this->request->filter(['strip_tags', 'trim']);
+        if (false === $this->request->isAjax()) {
+            return $this->view->fetch();
+        }
+        //如果发送的来源是 Selectpage，则转发到 Selectpage
+        if ($this->request->request('keyField')) {
+            return $this->selectpage();
+        }
+        [$where, $sort, $order, $offset, $limit] = $this->buildparams();
+        $list = $this->model
+            ->where($where)
+            ->where('pay_status','<>',0)
+            ->order($sort, $order)
+            ->paginate($limit);
+        $result = ['total' => $list->total(), 'rows' => $list->items()];
+        return json($result);
+    }
 
     /**
      * 充值
