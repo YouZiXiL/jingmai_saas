8db3e1146a617b9281303d1167ce87c12de6b42a
dong865 <865832@qq.com>
2023-05-11 15:34:30 +0800

    智能下单

---
+++ application/admin/controller/open/Order.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\admin\controller\open;
+
+use app\common\controller\Backend;
+use think\Controller;
+use think\Request;
+
+class Order extends Backend
+{
+    /**
+     *  智能下单
+     */
+    public function index()
+    {
+        //
+    }
+    /**
+     *  下单
+     */
+    public function create()
+    {
+        //
+    }
+
+}
