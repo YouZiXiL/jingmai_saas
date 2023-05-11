5d1e5ac81787f4a37ec9ea366bb8f712f654cd4f
dong865 <865832@qq.com>
2023-05-11 19:06:16 +0800

    智能下单

--- application/admin/controller/open/Order.php
+++ application/admin/controller/open/Order.php
@@ -5,10 +5,19 @@ namespace app\admin\controller\open;
 use app\common\business\YunYang;
 use app\common\controller\Backend;
 use think\Controller;
+use think\Exception;
 use think\Request;
 
 class Order extends Backend
 {
+    /**
+     *  智能下单
+     * @throws Exception
+     */
+    public function index()
+    {
+        return $this->view->fetch();
+    }
 
     /**
      *  下单
